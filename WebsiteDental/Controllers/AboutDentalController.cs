e052f03310ff31d845a6666acd4014b008de91a6
Nghia2812 <letrongnghia342@gmail.com>
2025-03-24 16:44:02 +0700

    ĐẨY CODE LÊN

---
+++ WebsiteDental/Controllers/AboutDentalController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebsiteDental.Controllers
+{
+    public class AboutDentalController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
