a1c70e76e3e0b8c6fbf8de607efdbf3fcaab3530
Nghia2812 <letrongnghia342@gmail.com>
2025-03-25 01:35:55 +0700

    code thêm ạ

--- WebsiteDental/Controllers/AboutDentalController.cs
+++ WebsiteDental/Controllers/AboutDentalController.cs
@@ -1,11 +1,28 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using WebsiteDental.Models;
 
 namespace WebsiteDental.Controllers
 {
     public class AboutDentalController : Controller
     {
+        private readonly WebsiteDentalContext _context;
+
+        public AboutDentalController(WebsiteDentalContext context)
+        {
+            _context = context;
+        }
         public IActionResult Index()
         {
+
+            // Lấy danh sách ảnh từ bảng ImageGallery
+            var imageGallery = _context.ImageGalleries.ToList();
+            // Lấy dữ liệu từ bảng AboutDental
+            var aboutDental = _context.AboutDentals.FirstOrDefault(); // Lấy 1 bản ghi
+
+            // Truyền dữ liệu sang View
+            ViewBag.ImageGallery = imageGallery;
+            ViewBag.AboutDental = aboutDental;
             return View();
         }
     }
---
+++ WebsiteDental/Controllers/BlogPostsController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WebsiteDental.Models;
+using Microsoft.EntityFrameworkCore;
+
+public class BlogPostsController : Controller
+{
+    private readonly WebsiteDentalContext _context;
+
+    public BlogPostsController(WebsiteDentalContext context)
+    {
+        _context = context;
+    }
+
+
+    public async Task<IActionResult> Index()
+    {
+        var blogPosts = await _context.BlogPosts.ToListAsync();
+        return View();
+    }
+}
