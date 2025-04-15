02f12c586bf696f21a245901547853b6fdcbc0a8
Nghia2812 <letrongnghia342@gmail.com>
2025-04-15 23:07:37 +0700

    code phần chi tiết sản phảm

--- WebsiteDental/Controllers/DetailProductsController.cs
+++ WebsiteDental/Controllers/DetailProductsController.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using WebsiteDental.Models;
+using System.Linq;
 
 namespace WebsiteDental.Controllers
 {
@@ -11,10 +12,33 @@ namespace WebsiteDental.Controllers
         {
             _context = context;
         }
+
         public IActionResult Index(int id)
         {
-            // Lấy chi tiết sản phẩm theo id và truyền ra View
-            return View();
+            // Lấy thông tin sản phẩm theo id
+            var product = _context.Products
+                .FirstOrDefault(p => p.Id == id);
+
+            if (product == null)
+            {
+                return NotFound();
+            }
+
+            // Lấy danh mục sản phẩm nếu cần (nếu có quan hệ khóa ngoại)
+            var categoryList = _context.ProductCategories.ToList();
+
+            // Lấy chi tiết sản phẩm (nếu có bảng riêng)
+            var productDetail = _context.ProductDetails
+                .FirstOrDefault(d => d.ProductId == id);
+
+            // Tạo ViewModel để truyền ra View
+            var viewModel = new ProductDetailModelView
+            {
+                Product = product,
+                CategoryList = categoryList, // Truyền danh sách danh mục
+                Detail = productDetail
+            };
+            return View(viewModel);
         }
     }
 }
